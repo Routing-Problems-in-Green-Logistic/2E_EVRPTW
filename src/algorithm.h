94430a06f3be3e2c3bf9c58bdc7af5a744ebb2d9
Samuel Abreu <samuelapp@outlook.com>
2021-08-13 00:08:06 -0300

    feat: add algorithm header

---
+++ src/algorithm.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Solution.h"
+#include "Instance.h"
+
+/** Checks the feasibility of the Solution, returning false if infeasible.
+ *
+ * @param Sol Solution
+ * @param Inst Instance
+ * @return true if feasible
+ */
+bool isFeasibleSolution(Solution& Sol, Instance& Inst);
