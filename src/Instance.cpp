63e58462739a59710ab5aab060962f7808bcb7c6
Samuel Abreu <samuelapp@outlook.com>
2021-08-12 23:59:42 -0300

    feat: Add getters and Setters to Instance class.

--- src/Instance.cpp
+++ src/Instance.cpp
@@ -1,1 +1,53 @@
 #include "Instance.h"
+
+Instance::Instance(std::vector<std::vector<double>> &distMat, float truckCap, float evCap, float evBattery, int nSats,
+                   int nClients, int nRS, std::vector<std::pair<float, float>> &coordinates,
+                   std::vector<float> &demands) {
+    this->demands = demands;
+    this->coordinates = coordinates;
+    this->distMat = distMat;
+    this->truckCap = truckCap;
+    this->evCap = evCap;
+    this->evBattery = evBattery;
+    this->nRS = nRS;
+    this->nClients = nClients;
+    this->nSats = nSats;
+}
+
+// Getters and Setters
+float Instance::getDemand(int node) const {
+    return this->demands[node];
+}
+
+float Instance::getTruckCap() const {
+    return truckCap;
+}
+
+float Instance::getEvCap() const {
+    return evCap;
+}
+
+float Instance::getEvBattery() const {
+    return evBattery;
+}
+
+int Instance::getNSats() const {
+    return nSats;
+}
+
+int Instance::getNClients() const {
+    return nClients;
+}
+
+int Instance::getNrs() const {
+    return nRS;
+}
+
+float Instance::getDistance(int n1, int n2) const {
+    return (float)this->distMat[n1][n2];
+}
+
+int Instance::getFirstClientIndex() const {
+    return (int)this->demands.size() - this->getNClients(); // the last nodes are clients;
+
+}
--- src/Instance.h
+++ src/Instance.h
@@ -7,17 +7,21 @@
 class Instance{
 public:
     Instance(std::vector<std::vector<double>>& distMat, float truckCap, float evCap, float evBattery,
-             int nSats, int nClients, int nRS, std::vector<std::pair<float,float>>& coordinates, std::vector<float>& demands){
-        this->demands = demands;
-        this->coordinates = coordinates;
-        this->distMat = distMat;
-        this->truckCap = truckCap;
-        this->evCap = evCap;
-        this->evBattery = evBattery;
-        this->nRS = nRS;
-        this->nClients = nClients;
-        this->nSats = nSats;
-    }
+             int nSats, int nClients, int nRS, std::vector<std::pair<float,float>>& coordinates, std::vector<float>& demands);
+    float getDemand(int node) const; // the const means that the method promises not to alter any members of the class.
+                                    // https://stackoverflow.com/questions/751681/meaning-of-const-last-in-a-function-declaration-of-a-class
+    float getDistance(int n1, int n2) const;
+    int getFirstClientIndex() const;
+    float getTruckCap() const;
+    float getEvCap() const;
+    float getEvBattery() const;
+    int getNSats() const;
+    int getNClients() const;
+    /** gets the number of Recharging Stations.
+     * @return number of Recharging Stations
+     */
+    int getNrs() const;
+
 private:
     std::vector<std::vector<double>> distMat;
     std::vector<float> demands;
