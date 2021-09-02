3e71d4eda1c891e22f6191d4ff9d1f6dca586d69
Samuel Abreu <samuelapp@outlook.com>
2021-09-02 16:33:46 -0300

    feat: add cost to solution

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -4,10 +4,11 @@ Solution::Solution() {
     nEvs = 0;
     nTrucks = 0;
 }
-Solution::Solution(std::vector<std::vector<int>> &routes, int nTrucks, int nEvs){
+Solution::Solution(std::vector<std::vector<int>> &routes, int nTrucks, int nEvs, float cost){
     this->routes = routes; // TODO: see if this actually works
     this->nTrucks = nTrucks;
     this->nEvs = nEvs;
+    this->cost = cost;
 }
 
 int Solution::getNTrucks() const {
@@ -20,4 +21,8 @@ int Solution::getNEvs() const {
 
 const std::vector<std::vector<int>>& Solution::getRoutes() {
     return routes;
+}
+
+float Solution::getCost() const {
+    return cost;
 }
--- src/Solution.h
+++ src/Solution.h
@@ -7,11 +7,15 @@ private:
     std::vector<std::vector<int>> routes;
     int nTrucks;
     int nEvs;
+    float cost;
 public:
     Solution();
-    Solution(std::vector<std::vector<int>>& routes, int nTrucks, int nEvs);
+    Solution(std::vector<std::vector<int>>& routes, int nTrucks, int nEvs, float cost);
     int getNTrucks() const;
     int getNEvs() const;
+
+    float getCost() const;
+
     const std::vector<std::vector<int>>& getRoutes();
 };
 
