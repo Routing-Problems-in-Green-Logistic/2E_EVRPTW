b7bdcd640a7d8d3fe32965f96e68dbe0c729f9db
junqueira200 <junqueira200@gmail.com>
2021-11-19 15:10:04 -0300

    Troquei set de unvisitedClients e restrictedList para vector e list.

---
+++ src/LocalSearch.h
@@ -0,0 +1,56 @@
+//
+// Created by igor on 19/11/2021.
+//
+
+#ifndef INC_2E_EVRP_LOCALSEARCH_H
+#define INC_2E_EVRP_LOCALSEARCH_H
+
+#include "EvRoute.h"
+
+
+#define MOV_SHIFIT      0
+#define MOV_SWAP        1
+#define MOV_2_OPT       2
+#define MOV_CROSS       3
+
+namespace NS_LocalSearch
+{
+
+    class LocalSearch
+    {
+
+    public:
+
+        bool satellites2    = false;
+        int idSat0          = -1;
+        int idSat1          = -1;
+
+        bool interRoutes    = false;
+        int mov             = -1;
+
+        // Shifit: insert0 -> insert1
+        Insertion inser0;
+        Insertion inser1;
+
+    };
+
+}
+
+#endif //INC_2E_EVRP_LOCALSEARCH_H
+
+/*
+ * Movimentos:
+ *
+ * Entre satellites, intra e inter rotas
+ * -Swap        <- Samuel
+ * -Shift       <- Igor
+ *
+ * intra rota
+ * -2 opt       <- Samuel
+ *
+ * Entre satellites, 'inter rotas'
+ * -cross       <- Igor
+ *
+ *
+ *
+ */
